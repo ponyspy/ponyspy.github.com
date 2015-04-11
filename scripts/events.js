337125f8bb8e1f76fa48081ec4f90967df127e06
ponyspy <desade4me@gmail.com>
2015-04-11 21:56:13 +0300

    cache

--- scripts/events.js
+++ scripts/events.js
@@ -1,4 +1,11 @@
 $(document).ready(function() {
+
+	$('.event_item_block').each(function() {
+		var img = new Image();
+		img.src = $(this).attr('image');
+	});
+
+
 	$('.event_item_block').on({
 		mouseenter: function(event) {
 			var path = $(this).attr('image');
