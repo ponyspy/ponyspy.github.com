56c47feff5e55587733993d26a9dae6fb5da9905
ponyspy <desade4me@gmail.com>
2015-04-26 21:31:36 +0300

    present

---
+++ scripts/main.js
@@ -0,0 +1,61 @@
+$(document).ready(function() {
+	var as = audiojs.createAll()[0];
+	$('.people_block').data('play', false).attr('play', 'false')
+
+	function selectPeople(index) {
+		var $people_nav = $('.people_nav');
+		var audio = $people_nav.eq(index).attr('audio-src');
+		as.load(audio);
+		as.play();
+
+		$people_nav
+			.children('.people_photo').removeClass('selected').end()
+			.eq(index).children('.people_photo').addClass('selected');
+	}
+
+
+	function scrollSelect() {
+		$('.people_block').each(function() {
+			var $this = $(this);
+
+			if ($('.main').offset().top >= $this.offset().top + $this.height()) {
+				$this.data('play', false).attr('play', 'false');
+			} else if ($('.main').offset().top >= $this.offset().top && !$this.data('play')) {
+				var index = $this.index();
+				$this.data('play', true).attr('play', 'true');
+
+				selectPeople(index);
+			}
+
+		});
+	}
+
+	$('.people_nav').on('click', function(event) {
+		$('.main').off('scroll');
+
+		var index = $(this).index('.people_nav');
+		$('.people_block').data('play', false).attr('play', 'false').eq(index).data('play', true).attr('play', 'true');
+
+		selectPeople(index);
+
+		$('.main').animate({
+			'scrollTop': $('.people_block').eq(index).offset().top + $('.main').scrollTop()
+		}, 600, function() {
+			$('.main').on('scroll', scrollSelect);
+		});
+	});
+
+	$('.main').on('scroll', scrollSelect);
+
+	$('.fullscreen').on('click', function(event) {
+		var target = $('.columns_block')[0];
+		if (screenfull.enabled) {
+			screenfull.request(target);
+		}
+	});
+
+	// $(document).on('contextmenu', function() {
+	// 	return false;
+	// });
+
+});
