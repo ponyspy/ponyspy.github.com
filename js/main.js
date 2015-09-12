d09b404f70dc569ab8bb07cd00a690e06e94dd88
ponyspy <desade4me@gmail.com>
2015-09-12 19:29:51 +0300

    timer

--- js/main.js
+++ js/main.js
@@ -1,11 +1,19 @@
 $(document).ready(function() {
+	var flip_timer = null;
+
 	$('.flip_block').on('touchstart mouseenter', function(event) {
 		var $this = $(this);
 
 		$this.toggleClass('hover');
-		setTimeout(function() {
-			$this.removeClass('hover');
-		}, 4000);
+		flip_timer = setTimeout(function() {
+			$this.toggleClass('hover');
+			flip_timer = null;
+		}, 3000);
 	});
+
+	setInterval(function() {
+		if (flip_timer) return false;
+		$('.flip_block').toggleClass('hover');
+	}, 3000);
 
 });
