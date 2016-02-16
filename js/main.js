732549bff2789874ffc7ee71154fbafe67b29994
ponyspy <desade4me@gmail.com>
2016-02-16 14:35:12 +0300

    mobile

--- js/main.js
+++ js/main.js
@@ -2,16 +2,6 @@ $(document).ready(function() {
 
 	$(document)
 		.on('scroll', function() {
-			var $this = $(this);
-			var scroll_top = $this.scrollTop();
-			var body_offset = $('.content_body').offset().top;
-			var document_height = $this.height();
-			var scroll_persent = scroll_top / (document_height - body_offset) * 100;
-
-			scroll_persent >= 0
-				? $('.photo_inner').css('background-size', 100 + scroll_persent + '%')
-				: false;
-
 			$(window).scrollTop() + $(window).height() + 600 >= $(document).height()
 				? $('.other').slice(0, 6).clone().insertAfter('.other:last')
 				: false;
@@ -22,5 +12,11 @@ $(document).ready(function() {
 
 			$('.desc_select').removeClass('active').filter(this).addClass('active');
 			$('.item_desc').removeClass('active').eq(index).addClass('active');
+		});
+
+		$('.photo_inner').on('click', function() {
+			$('body').animate({
+				'scrollTop': $('.content_item').eq(0).offset().top - $('.menu_block').height() - 15
+			}, 400);
 		});
 });
