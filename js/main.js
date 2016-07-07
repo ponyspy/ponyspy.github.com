2aa31d1a2b8b698a82a1b2b2604b2a6b63c2c9d4
ponyspy <desade4me@gmail.com>
2016-07-07 14:14:46 +0300

    init

--- js/main.js
+++ js/main.js
@@ -1,18 +1,30 @@
-$(document).ready(function() {
-	$('.images_block').masonry({
-	  itemSelector: '.brick',
-	  columnWidth: 180,
-	  gutter: 20
+$(function() {
+
+	var swiperH = new Swiper('.swiper-container-h', {
+			nextButton: '.swiper-button-next',
+			prevButton: '.swiper-button-prev',
+			keyboardControl: true,
+			spaceBetween: 0,
+			loop: true,
+			initialSlide: 0,
+			speed: 600,
+			longSwipesRatio: 0.2,
 	});
 
-	$('.projects_block').masonry({
-	  itemSelector: '.project',
-	  columnWidth: 180,
-	  gutter: 20
+	var swiperV = new Swiper('.swiper-container-v', {
+			keyboardControl: true,
+			direction: 'vertical',
+			speed: 600,
+			longSwipesRatio: 0.2,
+			spaceBetween: 0,
+			onSlideChangeEnd: function(swiper) {
+				swiperV.forEach(function(item, index) {
+					item.slideTo(swiper.activeIndex, 0, false);
+				});
+			}
 	});
 
-	$('.drop_item').on('click', function() {
-		$(this).toggleClass('open');
-	});
-
+	// $(document).on('contextmenu', function(e) {
+	// 	e.preventDefault();
+	// });
 });
