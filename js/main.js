db9690a725fb238af7126bd919bbca06fc02f6b6
ponyspy <desade4me@gmail.com>
2016-12-01 23:30:09 +0300

    new

--- js/main.js
+++ js/main.js
@@ -2,14 +2,4 @@ $(function() {
 	$('.cv').on('click', function(e) {
 		window.open('cv.html', '', 'width=360,height=500,left=200,top=200');
 	});
-
-	$('.contact').on('change', function(e) {
-		var selected = $('.contact option:selected').text();
-
-		if (selected == 'email') {
-			location.href = 'mailto:wassia@kuku.com';
-		} else if (selected == 'fb') {
-			window.open('http://www.facebook.com', '_blank');
-		}
-	});
 });
