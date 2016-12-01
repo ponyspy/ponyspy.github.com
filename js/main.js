9272277050f21add79fa29b1b939635e3a29a907
ponyspy <desade4me@gmail.com>
2016-12-01 20:30:41 +0300

    cv

--- js/main.js
+++ js/main.js
@@ -1,3 +1,15 @@
 $(function() {
+	$('.cv').on('click', function(e) {
+		window.open('cv.html', '', 'width=360,height=500,left=200,top=200');
+	});
 
+	$('.contact').on('change', function(e) {
+		var selected = $('.contact option:selected').text();
+
+		if (selected == 'email') {
+			location.href = 'mailto:wassia@kuku.com';
+		} else if (selected == 'fb') {
+			window.open('http://www.facebook.com', '_blank');
+		}
+	});
 });
