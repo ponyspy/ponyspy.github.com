634960e989ea98653f7d7ce51afcfa54a4c7eddc
ponyspy <desade4me@gmail.com>
2016-12-05 16:19:20 +0300

    new

--- js/main.js
+++ js/main.js
@@ -1,5 +1,48 @@
+var current = 1;
+var db = [
+	{
+		path: 'img/1.jpg',
+		desc: '2016 / pokushal / privet wassia! privet wassia! privet wassia!'
+	},
+	{
+		path: 'img/2.jpg',
+		desc: '2015 / pospal / dlinno dlinno dlinno'
+	},
+	{
+		path: 'img/3.jpg',
+		desc: '2014 / ugadal / medlenno medlenno medlenno'
+	},
+	{
+		path: 'img/4.gif',
+		desc: '2013 / vstal / mashina / chi'
+	},
+	{
+		path: 'img/5.jpg',
+		desc: '2012 / cel / kokoko lalala'
+	},
+	{
+		path: 'img/6.jpg',
+		desc: '2012 / bee / um mu um mu'
+	}
+];
+
 $(function() {
 	$('.cv').on('click', function(e) {
 		window.open('cv.html', '', 'width=360,height=500,left=200,top=200');
+	});
+
+	$('body').on('click', function(e) {
+		$('<div>', {'class': 'dot'}).css({'left': e.pageX - 10, 'top': e.pageY - 10}).appendTo('body');
+	});
+
+	$('img').on('click', function() {
+		$('img').attr('src', db[current].path);
+		$('.marquee').children('span').text(db[current].desc);
+
+		$('.current').text(current + 1);
+
+		current == db.length - 1
+			? current = 0
+			: current += 1;
 	});
 });
