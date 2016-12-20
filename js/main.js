b9af75d10960dde3c1c1946095737b37dbb867a2
ponyspy <desade4me@gmail.com>
2016-12-20 20:04:00 +0300

    bird

--- js/main.js
+++ js/main.js
@@ -1,48 +1,13 @@
-var current = 1;
-var db = [
-	{
-		path: 'img/1.jpg',
-		desc: '2016 / pokushal / privet wassia! privet wassia! privet wassia!'
-	},
-	{
-		path: 'img/2.jpg',
-		desc: '2015 / pospal / dlinno dlinno dlinno'
-	},
-	{
-		path: 'img/3.jpg',
-		desc: '2014 / ugadal / medlenno medlenno medlenno'
-	},
-	{
-		path: 'img/4.gif',
-		desc: '2013 / vstal / mashina / chi'
-	},
-	{
-		path: 'img/5.jpg',
-		desc: '2012 / cel / kokoko lalala'
-	},
-	{
-		path: 'img/6.jpg',
-		desc: '2012 / bee / um mu um mu'
-	}
-];
-
 $(function() {
-	$('.cv').on('click', function(e) {
-		window.open('cv.html', '', 'width=360,height=500,left=200,top=200');
-	});
-
-	$('img').mousedown(function(){
-		return false;
-	});
-
-	$('img').on('click', function() {
-		$('img').attr('src', db[current].path);
-		$('.marquee').children('span').text(db[current].desc);
-
-		$('.current').text(current + 1);
-
-		current == db.length - 1
-			? current = 0
-			: current += 1;
+	var swiperH = new Swiper('.swiper-container-h', {
+		slidesPerView: 1,
+		direction: 'horizontal',
+		spaceBetween: 30,
+		// autoHeight: true,
+		// initialSlide: 1,
+		centeredSlides: true,
+		keyboardControl: true,
+		loop: true,
+		runCallbacksOnInit: false,
 	});
 });
