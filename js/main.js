f3d111d8930ebffcaa42a1cc49bbb7c6e90c6861
ponyspy <desade4me@gmail.com>
2016-12-21 12:11:48 +0300

    new

--- js/main.js
+++ js/main.js
@@ -2,7 +2,7 @@ $(function() {
 	var swiperH = new Swiper('.swiper-container-h', {
 		slidesPerView: 1,
 		direction: 'horizontal',
-		spaceBetween: 30,
+		spaceBetween: 20,
 		speed: 600,
 		autoplay: 4000,
 		// autoplayDisableOnInteraction: false,
