5e37ec1cd2f3f08ba1c1cb96ab1ecf21611c1d02
ponyspy <desade4me@gmail.com>
2017-01-19 20:55:00 +0300

    bc

---
+++ js/main.js
@@ -0,0 +1,17 @@
+$(function() {
+	var swiperH = new Swiper('.swiper-container-h', {
+		slidesPerView: 1,
+		direction: 'horizontal',
+		spaceBetween: 20,
+		speed: 800,
+		autoplay: 4000,
+		// autoplayDisableOnInteraction: false,
+		// effect: 'fade',
+		// autoHeight: true,
+		// initialSlide: 1,
+		centeredSlides: true,
+		keyboardControl: true,
+		loop: true,
+		runCallbacksOnInit: false,
+	});
+});
--- js/video.js
+++
@@ -1,5 +0,0 @@
-$(function() {
-	var back_interval = setInterval(function() {
-		window.location.href = '../index.html';
-	}, 1000 * 60 * 12);
-});
