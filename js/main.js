d73fe219ba4b80aeb61eb8dfc927e209a723cc77
ponyspy <desade4me@gmail.com>
2018-05-29 23:07:14 +0300

    fix

--- js/main.js
+++ js/main.js
@@ -2,10 +2,12 @@ $(function() {
 	$('.d_item').on('mouseenter', function(e) {
 		var id = $(this).attr('class').split(' ')[1];
 
-		$('.p_item').not('.' + id).addClass('hover');
+		$('.p_item').filter('.' + id).addClass('select');
+
+		$('.p_item, .d_item').not('.' + id).addClass('hover');
 	});
 
 	$('.d_item').on('mouseleave', function(e) {
-		$('.p_item').removeClass('hover');
+		$('.p_item, .d_item').removeClass('hover select');
 	});
 });
