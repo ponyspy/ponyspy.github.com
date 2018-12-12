adef1004f61bff7ebd06543b04e4a1981003656d
ponyspy <desade4me@gmail.com>
2018-12-13 00:34:53 +0300

    shake

--- js/main.js
+++ js/main.js
@@ -1,11 +1,34 @@
 $(function() {
-	$('.d_item').on('mouseenter', function(e) {
-		var id = $(this).attr('class').split(' ')[1];
+	if (typeof window.DeviceMotionEvent != 'undefined') {
+			// Shake sensitivity (a lower number is more)
+			var sensitivity = 30;
 
-		$('.p_item, .d_item').not('.' + id).addClass('hover');
-	});
+			// Position variables
+			var x1 = 0, y1 = 0, z1 = 0, x2 = 0, y2 = 0, z2 = 0;
 
-	$('.d_item').on('mouseleave', function(e) {
-		$('.p_item, .d_item').removeClass('hover');
-	});
+			// Listen to motion events and update the position
+			window.addEventListener('devicemotion', function (e) {
+					x1 = e.accelerationIncludingGravity.x;
+					y1 = e.accelerationIncludingGravity.y;
+					z1 = e.accelerationIncludingGravity.z;
+			}, false);
+
+			// Periodically check the position and fire
+			// if the change is greater than the sensitivity
+			setInterval(function () {
+					var change = Math.abs(x1-x2+y1-y2+z1-z2);
+
+					if (change > sensitivity) {
+						$('.image').addClass('show');
+							setTimeout(function() {
+								$('.image').removeClass('show');
+							}, 2000);
+					}
+
+					// Update new position
+					x2 = x1;
+					y2 = y1;
+					z2 = z1;
+			}, 150);
+	}
 });
