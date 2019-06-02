909e285c85531515d85a2b0a6ef59dca471b5672
ponyspy <desade4me@gmail.com>
2019-06-03 00:26:35 +0300

    fix

--- js/main.js
+++ js/main.js
@@ -1,16 +1,18 @@
-$(function() {
-	$(window).on('load hashchange', function(e) {
-		$('.drop').removeClass('show').filter(location.hash).addClass('show');
-	});
+$(window).on('load hashchange', function(e) {
+	$('.drop').removeClass('show').filter(location.hash).addClass('show');
+});
 
+$(function() {
 	$('.main_block').on('mousemove', function(e) {
 		$('.content_line').css('left', e.pageX - 2);
 	});
 
 	$('.mask_item').on('mouseenter', function(e) {
-		var index = $(this).index();
+		var $this = $(this);
+		var index = $this.index();
 
-		$('.title_block').text($(this).attr('data-title'));
+		$('.title_block').text($this.attr('data-title'));
 		$('.content_item').removeClass('active').eq(index).addClass('active');
+		$('.mask_item').removeClass('active').eq(index).addClass('active');
 	})
 });
