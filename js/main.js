2f75993d46d4f7429ab12831d6d67413b2ea9c6d
ponyspy <desade4me@gmail.com>
2021-01-28 21:42:18 +0300

    scroll

--- js/main.js
+++ js/main.js
@@ -1,5 +1,21 @@
 $(function() {
 
+  $('.column_inner').on('mouseenter', function(e) {
+    var $this = $(this);
+
+    $('.column_inner').off('scroll').filter(this).on('scroll', function(e) {
+      // var scroll_persentage = 100 * ($this.scrollTop() + $this.height()) / this.scrollHeight;
+      var scroll_persentage = $this.scrollTop() / (this.scrollHeight - $this.height()) * 100;
+
+      console.log(scroll_persentage)
+
+      $('.column_inner').not(this).each(function() {
+        // $(this).scrollTop($this.scrollTop() / 100 * scroll_persentage);
+        $(this).scrollTop(scroll_persentage * (this.scrollHeight - $(this).height()) / 100);
+      });
+    })
+  });
+
   var swColumns = new Swiper('.swiper-columns', {
     spaceBetween: 0,
     simulateTouch: false,
